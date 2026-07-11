5e6fd83052d04cca2ddd130aff32f6785779567d
kkambo <151587265+whtjdrud@users.noreply.github.com>
2026-07-11 23:25:24 +0900

    Refactor solution method for even and odd handling

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -1,19 +1,20 @@
 class Solution {
     public int solution(int n) {
-        
         int answer = 0;
-        boolean isEven = n % 2 == 0;
         
-        
-        for(int i = 0; i <= n ; i++){            
-            if(i % 2 == 0 && isEven){
-                answer += i * i;
+        if (n % 2 == 0) {
+            for (int i = 0; i <= n; i+=2) {
+                if (i % 2 == 0) {
+                    answer += i * i;
+                }
             }
-            if(i % 2 == 1 && !isEven)            
-            {
-                answer += i;
+        } else {
+            for (int i = 1; i <= n; i+=2) {
+                if (i % 2 == 1) {
+                    answer += i;
+                }
             }
-        }        
+        }
         
         return answer;
     }
